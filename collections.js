61e2b25da84338f260304c56c4a4042986f97211
Edward Okech <okechjobs@gmail.com>
2015-08-31 02:38:20 +0300

    added collection file

---
+++ collections.js
@@ -0,0 +1,16 @@
+/**
+ * Created by princek on 8/30/15.
+ */
+Categories = new Mongo.Collection('categories');
+
+//make a default name for a list in the for of 'Category A'
+Categories.defaultName = function(){
+    var nextLetter = 'A', nextGenre = 'List' + nextLetter;
+    while(Categories.findOne({genre: nextGenre})){
+        nextLetter = String.fromCharCode(nextLetter.charCodeAt(0) + 1);
+        nextGenre = 'Category' + nextLetter;
+    }
+    return nextGenre;
+};
+Movies = new Mongo.Collection('movies');
+
