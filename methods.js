3351420f48cb304bbdee97f70168ebd1fb497600
Edward Okech <okechjobs@gmail.com>
2015-09-01 16:24:58 +0300

    style changes to the template

--- methods.js
+++ methods.js
@@ -2,9 +2,16 @@
  * Created by princek on 8/31/15.
  */
 Meteor.methods({
-    insertMovie: function(movieDocument){
-        if(this.isSimulation){
+    insertMovie: function (movieDocument) {
+        if (this.isSimulation) {
             Session.set('Save Button', 'Saving...');
         }
+        else {
+            var user = Meteor.user();
+
+            if (user) {
+                throw new Meteor.Error(401, 'you need to login to add a movie');
+            }
+        }
     }
-})
+});
